019c61cc03112ca58f1abeb9c0eda5cfbcc74c52
Marco Treglia <marcotreglia1@gmail.com>
2018-01-29 20:53:51 +0100

    dist created. and now is online in PyPI

--- mark_utils/setup.py
+++ mark_utils/setup.py
@@ -1,12 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(name="mark_utils",
-      version="0.1",
+      version="0.0.3",
       description="Some MachineLearning Utils",
       author="Marco Treglia",
       author_email='marcotreglia1@gmail.com',
       platforms=["any"],
       license="BSD",
-      url="http://github.com/ewencp/foo",
+      url="https://github.com/MarkNo1/Utils",
+      download="",
       packages=find_packages(),
       )
